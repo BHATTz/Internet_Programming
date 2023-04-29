6402b41dad655a40bb9d5a5288c3ed4084e3ce79
SARTHAK BHATT <87993262+BHATTz@users.noreply.github.com>
2023-04-29 17:38:30 +0530

    Add files via upload

---
+++ P1/src/java/NewServlet.java
@@ -0,0 +1,89 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NewServlet extends HttpServlet {
+
+        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //read from fields
+        String username=request.getParameter("username");
+        String lastname=request.getParameter("lastname");
+        
+        System.out.println("username:"+username);
+        System.out.println("Lastname:"+lastname);
+        //do some processing here        
+        //get response writer
+        PrintWriter writer=response.getWriter();
+        //build HTML code
+        String htmlRespone ="<html>"; 
+        htmlRespone+="<h2>Your username is:"+username+"<br/>";
+        htmlRespone+="Your lastname is:"+lastname+"</h2>";
+        htmlRespone+="<html>";
+        //return respone
+        writer.println(htmlRespone);
+                
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private static class PrintWrite {
+
+        public PrintWrite() {
+        }
+    }
+
+}
